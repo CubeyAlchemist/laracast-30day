b5ca5dc1810cf8d1d61152153ea3916d75c44b9c
Adam <adam.scatchard@gmail.com>
2025-07-05 14:15:01 +0100

    Part 4: Layout using Tailwind UI
    Used 'Stacked' layout from Tailwind with minor modifications. Updated nav-link blade with new format but still assuming PageName=FileName.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,4 @@
-<a href="/{{ strtolower($slot) }}">{{ $slot }}</a>
+<!-- replaced with tailwind UI based navlink. Still assuming that Page name = file name
+    <a href="/{{ strtolower($slot) }}">{{ $slot }}</a> 
+-->
+<a href="/{{ strtolower($slot) }}" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ $slot }}</a>
--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Teams Page
+    </x-slot:heading>
     <h1>This is the Teams Page</h1>
 </x-layout>
