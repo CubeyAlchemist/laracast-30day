c5cf139ebdf7ece8dcf507ece7d6f26e14a4d61f
Adam <adam.scatchard@gmail.com>
2025-07-05 17:09:22 +0100

    Part 9: Eloquent
    Using Eloquent to reduce complexity of class models and interact with database

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,23 +2,10 @@
 
 namespace App\Models;
 
-class Job 
-{
-    public static function all(): array
-    {
-        return [
-            ['id' => 1, 'title' => 'Director', 'salary' => 60000],
-            ['id' => 2, 'title' => 'Programmer', 'salary' => 20000],
-            ['id' => 3, 'title' => 'Teacher', 'salary' => 40000]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find(int $id): array
-    {
-        $job = collect(static::all())->first(fn($job) => $job['id'] == $id);
-        if (! $job){
-            abort(404);
-        }
-        return $job;
-    }
+class Job extends Model
+{
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
