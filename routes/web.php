da4e5b3f6cb5962465b0a3bc2ca6d1b40b33a7d1
Adam <adam.scatchard@gmail.com>
2025-07-06 14:24:06 +0100

    Part 18: Modifying resources
    Creating additional routes and validation for editing and deleting existing resources

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/contact', function() {
     return view('contact');
 });
 
+// Index
 Route::get('/jobs', function() {
     $jobs = Job::with('employer')->latest()->simplePaginate(5);
     return view('jobs.index', [
@@ -29,16 +30,19 @@ Route::get('/jobs', function() {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function() { 
     return view('jobs.create');
 });
 
+// Show
 // Note: Wildcards like '{id}' should appear after other explicit routes. Routes are processed in line-order.
 Route::get('/jobs/{id}', function($id) {
     $job = Job::find($id);
     return view ('jobs.show', ['job' => $job]);
 }); 
 
+// Store
 Route::post('/jobs', function(){
     // Validate inputs. On fail, automatically returns to form page and sends info on errors.
     request()->validate([
@@ -54,3 +58,35 @@ Route::post('/jobs', function(){
     ]);
     return redirect('/jobs');
 });
+
+// Edit
+Route::get('/jobs/{id}/edit', function($id) {
+    $job = Job::find($id);
+    return view ('jobs.edit', ['job' => $job]);
+}); 
+
+// Update
+Route::patch('/jobs/{id}', function($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // Authorise - TODO
+
+    $job = Job::findOrFail($id);    // gets the ID or 'fails' if unable to locate, preventing further execution
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('jobs/'.$job->id);
+}); 
+
+// Destroy
+Route::delete('/jobs/{id}', function($id) {
+    // Authorise - TODO
+    $job = Job::findOrFail($id);    // gets the ID or 'fails' if unable to locate, preventing further execution
+    $job->delete();
+    return redirect('/jobs');
+}); 
